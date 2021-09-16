10f5365ccf34240342a341f35b7372aff744daa7
ruslan <ruslanusd@gmail.com>
2021-09-16 13:30:34 +0300

    clean up the code

--- app.js
+++ app.js
@@ -9,5 +9,4 @@ app.get('/api/info', (req, res) => {
 app.post('/api/v1/getback', (req, res) => {
   res.send({ ...req.body });
 });
-//test comment aug 12 2021 17:09
-module.exports.handler = serverless(app);
+app.listen(3000, () => console.log(`Listening on: 3000`));
