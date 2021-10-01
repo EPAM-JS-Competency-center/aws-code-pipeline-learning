eb105926d55bf4526c69599c4d2c333ec4cdc22d
ruslan <ruslanusd@gmail.com>
2021-10-01 18:09:05 +0300

    fix node intallation

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 apt-get update
-apt-get install curl
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-apt-get install -y nodejs
+apt-get install ca-certificates
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 npm install forever -g
